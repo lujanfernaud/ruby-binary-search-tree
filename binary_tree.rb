60bcec95fa26399dc8242cbbdae3829a61581cd6
lujanfernaud <lujanfernaud@gmail.com>
2017-04-28 23:36:45 +0100

    Implement Tree#build and Node#insert

--- binary_tree.rb
+++ binary_tree.rb
@@ -1,10 +1,62 @@
+require "pry"
+
 class Node
   attr_accessor :value, :parent, :left, :right
 
-  def initialize(*args)
+  def initialize(args = default)
     @value  = args[:value]
     @parent = args[:parent]
     @left   = args[:left]
     @right  = args[:right]
   end
+
+  def default
+    { value: nil, parent: nil, left: nil, right: nil }
+  end
+
+  def insert(value)
+    return false if value == self.value
+
+    if value > self.value
+
+      if right.nil?
+        self.right = Node.new(value: value)
+      else
+        right.insert(value)
+      end
+
+    elsif value < self.value
+
+      if left.nil?
+        self.left = Node.new(value: value)
+      else
+        left.insert(value)
+      end
+
+    end
+  end
 end
+
+class Tree
+  attr_accessor :root
+
+  def initialize
+    @root = Node.new
+  end
+
+  def build(array)
+    @root.value = array[(array.length / 2).round]
+
+    array.each do |value|
+      @root.insert(value)
+    end
+  end
+end
+
+data = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324]
+
+tree = Tree.new
+tree.build(data)
+tree.root
+
+p tree
